cd2b0c3365617ca7c23cf91c9df2cc8fe00a2013
anickacodes <anickalewis@pursuit.org>
2024-06-24 00:45:07 -0400

    feat: styles queried & routes successful

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const morgan = require("morgan");
 const app = express();
 // const session = require("express-session");
 const categoryController = require("./controllers/categoryController")
+const stylesController = require("./controllers/stylesController")
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use("/category", categoryController);
+app.use("/styles", stylesController);
+
 // app.use(express.urlencoded({extended: true}))
 
 // app.use(
@@ -24,16 +28,6 @@ app.get("/", (req, res) => {
   );
 });
 
-// const styleController = require("./controllers/styleController.js");
-// const userController = require("./controllers/userController");
-
-// app.use("/styles", styleController);
-// app.use("/users", userController);
-
-//   app.post('/login', passport.authenticate('local', {
-//     successRedirect: '/',
-//     failureRedirect: '/login',
-//   }));
 
 app.get("*", (req, res) => {
   res.status(404).send("Page not found");
